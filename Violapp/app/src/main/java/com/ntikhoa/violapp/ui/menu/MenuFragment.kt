1a1d994dcb38f0aedbf74622f1d757808db55146
Khoa <ntikhoa123@gmail.com>
2021-03-30 22:10:03 +0700

    add menu

---
+++ Violapp/app/src/main/java/com/ntikhoa/violapp/ui/menu/MenuFragment.kt
@@ -0,0 +1,39 @@
+package com.ntikhoa.violapp.ui.menu
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.ntikhoa.violapp.R
+import com.ntikhoa.violapp.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment(R.layout.fragment_menu) {
+
+    private lateinit var navController: NavController
+
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMenuBinding.bind(view)
+        navController = Navigation.findNavController(view)
+
+        binding.button.setOnClickListener {
+            val action = MenuFragmentDirections.actionMenuFragmentToMetronomeFragment()
+            navController.navigate(action)
+        }
+
+        binding.button2.setOnClickListener {
+            val action = MenuFragmentDirections.actionMenuFragmentToSampleSoundFragment()
+            navController.navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ Violapp/app/src/main/java/com/ntikhoa/violapp/ui/sample_sound/SampleSoundFragment.kt
@@ -0,0 +1,8 @@
+package com.ntikhoa.violapp.ui.sample_sound
+
+import androidx.fragment.app.Fragment
+import com.ntikhoa.violapp.R
+
+
+class SampleSoundFragment : Fragment(R.layout.fragment_sample_sound) {
+}
